a44b5edba6442cee4411736238ab2aa48cebf924
FROGbots-4634 <teamfrogbots@gmail.com>
2017-12-22 08:53:21 -0500

    fix CSV append export

--- app/src/main/java/net/frogbots/relicrecoveryscorecalculator/backend/export/csv/CsvAddExport.java
+++ app/src/main/java/net/frogbots/relicrecoveryscorecalculator/backend/export/csv/CsvAddExport.java
@@ -24,16 +24,7 @@ public class CsvAddExport
         int count = readArray.length;
         String[][] out = new String[count+1][17];
         arrayCopy(readArray, out);
-
-        for(String[] s1 : out)
-        {
-            for (String s2 : s1)
-            {
-                System.out.println(s2);
-            }
-        }
-
-        CsvCommon.writeScoresToRow(out, exportBundle.comment, exportBundle.match, 1);
+        CsvCommon.writeScoresToRow(out, exportBundle.comment, exportBundle.match, count);
         CsvCommon.saveToCSV(exportBundle.activity, exportBundle.fileForCsvAdd, out);
     }
 
